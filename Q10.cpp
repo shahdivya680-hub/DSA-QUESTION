6e40983a27930f472292482d2794af882f052680
Divya Shah <shahdivya680@gmail.com>
2026-09-22 14:12:01 +0530

    Add files via upload
    Write a program to validate whether a given date represented by Day, Month, and Year is valid on the Gregorian calendar. Account for differing month lengths (30 vs 31 days) and leap years for February (28 vs 29 days).

--- Q10.cpp
+++ Q10.cpp
@@ -12,37 +12,37 @@ int main() {
     cout << "Enter Year: ";
     cin >> year;
 
-    // 2. Year aur Month check karna
-    if (year < 1) {
-        cout << "Invalid Date! (Year galat hai)" << endl;
-        return 0; // Program yahi khatam ho jayega
-    }
 
-    if (month < 1 || month > 12) {
-        cout << "Invalid Date! (Month galat hai)" << endl;
+    if (year < 1) {
+        cout << "Invalid Date! (enter right year)" << endl;
         return 0;
     }
 
-    // 3. Har mahine ke din tay karna (By default February me 28 din)
+    if (month < 1 || month > 12) {
+        cout << "Invalid Date! (enter right month)" << endl;
+        return 0;
+    }
+
+
     int maxDays = 31;
 
     if (month == 4 || month == 6 || month == 9 || month == 11) {
-        maxDays = 30; // April, June, September, November me 30 din hote hain
+        maxDays = 30; 
     } 
     else if (month == 2) {
-        // Leap year check karne ka simple tarika
+        
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            maxDays = 29; // Leap year me Feb me 29 din
+            maxDays = 29; 
         } else {
-            maxDays = 28; // Normal year me Feb me 28 din
+            maxDays = 28; 
         }
     }
 
-    // 4. Day check karna
+    
     if (day < 1 || day > maxDays) {
-        cout << "Invalid Date! (Is mahine me " << maxDays << " se zyada din nahi hote)" << endl;
+        cout << "Invalid Date! (In this month " << maxDays << " maximum)" << endl;
     } else {
-        cout << "Valid Date! (Aapki date bilkul sahi hai)" << endl;
+        cout << "Valid Date! (your date of birth is right)" << endl;
     }
 
     return 0;
